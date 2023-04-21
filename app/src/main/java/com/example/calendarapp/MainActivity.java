005e355a5cc354dc2af46a1e94ff3031fb53dd27
ysbllcby <ysabellacubay@gmail.com>
2023-04-21 15:56:36 -0600

    Firebase and libraries added.

--- app/src/main/java/com/example/calendarapp/MainActivity.java
+++ app/src/main/java/com/example/calendarapp/MainActivity.java
@@ -1,18 +1,47 @@
 package com.example.calendarapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CalendarView;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private CalendarView calendarView;
-    
+    private EditText editText;
+    private String stringDateSelected;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarView = findViewById(R.id.calendarView);
+        editText = findViewById(R.id.editText);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                stringDateSelected = Integer.toString(year) + Integer.toString(month) + Integer.toString(year);
+                calendarClicked();
+            }
+        });
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Calendar");
+    }
+
+    private void calendarClicked() {
+
+    }
+
+    public void saveButtonEvent(View view) {
+
     }
 }
